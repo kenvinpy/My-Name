1f48619bc7443b5497cc6d5f11577d63b2eb3daf
Tin Hoang Nguyen <kenvin.alone@gmail.com>
2014-12-22 08:56:59 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var orange = [40, 100, 60];
 var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
-
+/* Your name here*/
 var myName = "Hoang Tin";
 var letterColors = [red, orange, green, blue, purple];
 
